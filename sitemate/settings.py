05d6d9d4725c0434c00ef5de6c44d58eb66f2dc5
nilldiggonto <moinulantu@gmail.com>
2024-03-08 09:18:46 +0000

    frontend/client added

--- sitemate/settings.py
+++ sitemate/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
-    'djangorestframework',
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'sitemate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
