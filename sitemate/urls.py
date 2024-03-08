78c0ce9bf59cbf1510df0cd494677c3fe3c34037
nilldiggonto <moinulantu@gmail.com>
2024-03-08 09:56:33 +0000

    client list and create done

--- sitemate/urls.py
+++ sitemate/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from frontend.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage),
     path('api/', include('tasks.urls')),
 ]
